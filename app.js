f986d55627ae1a6eafcbe206db7a427e85be4c8c
M-ahmad144 <muhammadahmadmughal0987@gmail.com>
2025-01-12 21:55:14 +0500

    static file issue

--- app.js
+++ app.js
@@ -39,7 +39,14 @@ app.use(express.urlencoded({ extended: true }));
 
 // Static files
 app.use(express.static(path.join(__dirname, "public")));
-app.use("/uploads", express.static(path.join(__dirname, "./uploads")));
+
+if (process.env.NODE_ENV === "production") {
+  // For deployment environments
+  app.use("/uploads", express.static(path.resolve(__dirname, "../uploads")));
+} else {
+  // For local development
+  app.use("/uploads", express.static(path.join(__dirname, "./uploads")));
+}
 
 // Import and use routes
 app.use("/api/users", userRoutes);
